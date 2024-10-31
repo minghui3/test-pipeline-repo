9abfa7c669bf69571480afb201fe7cab95d6d469
minghui3 <s10257917@connect.np.edu.sg>
2024-10-31 21:22:13 +0800

    did the first 3 pages, half way through 4th

---
+++ public/scripts/script.js
@@ -0,0 +1,14 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username && password) {
+        alert("Login successful");
+        // Add further login functionality here
+        console.log(window.location.href)
+        window.location.href = "../views/directory.html";
+        console.log(window.location.href)
+    } else {
+        alert("Please enter both username and password.");
+    }
+}
