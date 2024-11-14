bec0de05bfcc0bd548d1067897dc3f09c60e72a0
minghui3 <149645776+minghui3@users.noreply.github.com>
2024-11-14 14:27:02 +0800

    Update LoginSteps.java

--- maven/src/test/java/stepdefinitions/LoginSteps.java
+++ maven/src/test/java/stepdefinitions/LoginSteps.java
@@ -28,7 +28,7 @@ public class LoginSteps
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        driver.get("http://localhost:3000");
+        driver.get("http://localhost:3001");
     }
     
 
